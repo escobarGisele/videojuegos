ce6202d86d3df21e309e9a8db0d57ce87a264e80
escobarGisele <escobargisele1@gmail.com>
2019-12-08 23:03:17 -0300

    agregado de service

--- src/main/java/com/example/videojuegos/controller/ListadoController.java
+++ src/main/java/com/example/videojuegos/controller/ListadoController.java
@@ -1,14 +1,29 @@
 
 package com.example.videojuegos.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.videojuegos.domain.Videojuego;
+import com.example.videojuegos.service.VideojuegosService;
 
 @Controller 
 public class ListadoController {
-    @RequestMapping("/")
-    public String ListarVideoJuegos(){
-    //buesqueda de video juegos 
+	public final VideojuegosService videojuegosService;
+	
+    public ListadoController(VideojuegosService videojuegosService) {
+		
+		this.videojuegosService = videojuegosService;
+	}
+
+	@RequestMapping("/")
+    public String ListarVideoJuegos(Model model){
+    //busqueda de video juegos 
+		List<Videojuego> destacados = videojuegosService.buscarDetacados();
+		model.addAttribute("videojuegos", destacados);
     return "listado"; 
     }
 }
--- src/main/java/com/example/videojuegos/service/VideojuegosService.java
+++ src/main/java/com/example/videojuegos/service/VideojuegosService.java
@@ -2,8 +2,41 @@ package com.example.videojuegos.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.videojuegos.domain.Videojuego;
+
+import java.util.ArrayList;
+import java.util.List;
 @Service
 
 public class VideojuegosService {
+    public List<Videojuego> buscarDetacados(){
+    	List<Videojuego> destacados= new ArrayList<>();
     
+    	Videojuego tetris= new Videojuego();
+    	tetris.setNombre("tetris");
+    	tetris.setDescripcion("es un juego de puzzles ");
+    	//falta url 
+    	destacados.add(tetris);
+    	
+    	Videojuego zelda= new Videojuego();
+    	zelda.setNombre("zelda");
+    	zelda.setDescripcion("juego de aventuras");
+    	//falta url 
+    	destacados.add(zelda);
+    	
+    	Videojuego needForSpeed= new Videojuego();
+    	needForSpeed.setNombre("needForSpeed");
+    	needForSpeed.setDescripcion("juego de aventuras");
+    	//falta url de imagen
+    	destacados.add(needForSpeed);
+    	
+    	Videojuego pokemon= new Videojuego();
+    	pokemon.setNombre("pokemon");
+    	pokemon.setDescripcion("juego de aventuras");
+    	//falta url de imagen 
+    	destacados.add(pokemon);
+    	
+		return destacados;
+        
+    }
 }
